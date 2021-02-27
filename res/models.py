1f80771f35230a77ad2c1f8078b8bdd22db08300
coding <ramstergun@gmail.com>
2021-02-27 14:59:45 +0900

    add default types

--- res/models.py
+++ res/models.py
@@ -1,24 +1,30 @@
 from typing import List, Optional
+from dataclasses import dataclass, field
 
 
+@dataclass
 class Mine:
-    length: int
-    width: int
-    map: List[List[int]]
+    length: int = 10
+    width: int = 10
+    map: List[List[int]] = field(default_factory=list(list))
 
 
+@dataclass
 class Backpack:
     slot1: Optional[int] = None
     slot2: Optional[int] = None
     slot3: Optional[int] = None
 
 
+@dataclass
 class Player:
-    xpos: int
-    ypos: int
-    back: Backpack
+    x_pos: int = 0
+    y_pos: int = 0
+    backpack: Backpack = field(default_factory=Backpack)
 
 
+@dataclass
 class Item:
     id: int
     name: str
+    hide: bool = True
